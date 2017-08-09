e76f374590697377ccd1b83ceb567654487dd7f7
yarsnowing <yarsnowing@outlook.com>
2017-08-09 21:26:02 +0800

    gg
    kk

--- 本书实例源文件/第11章 公交查询/MyBus/gen/com/guo/gongjiao/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.guo.gongjiao;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- 本书实例源文件/第13章 Google天气客户端/DomXML/gen/com/supermario/domxml/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.supermario.domxml;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
